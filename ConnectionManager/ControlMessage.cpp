e00e9f44c7beccdb8c151c50fdd8fad99d2ade1e
EmperorNao <59420696+EmperorNao@users.noreply.github.com>
2022-04-30 22:06:48 +0300

    Control Message for different behavior by instructions

---
+++ ConnectionManager/ControlMessage.cpp
@@ -0,0 +1,19 @@
+#include "ControlMessage.h"
+
+void network::DataMessage::save(std::string filename) const {
+
+    std::ofstream file(filename);
+    file.write(reinterpret_cast<char*>(type), sizeof(type));
+    file.write(reinterpret_cast<char*>(number_of_files), sizeof(number_of_files));
+    file.close();
+
+}
+
+void network::DataMessage::load(std::string filename) const {
+
+    std::ifstream file(filename);
+    file.read(reinterpret_cast<char*>(type), sizeof(type));
+    file.read(reinterpret_cast<char*>(number_of_files), sizeof(number_of_files));
+    file.close();
+
+}
---
+++ ConnectionManager/ControlMessage.h
@@ -0,0 +1,49 @@
+#ifndef DISTRIBUTEDCOMPUTINGSYSTEM_CONTROLMESSAGE_H
+#define DISTRIBUTEDCOMPUTINGSYSTEM_CONTROLMESSAGE_H
+
+
+#include <iostream>
+#include <fstream>
+
+
+namespace network {
+
+    enum class MessageType {DataMessage};
+
+
+    class ControlMessage {
+
+    protected:
+        MessageType type;
+
+    public:
+        explicit ControlMessage(MessageType _type): type(_type) {}
+        virtual void save(std::string filename) const = 0;
+        virtual void load(std::string filename) const = 0;
+
+        [[nodiscard]] MessageType get_type() const { return type; }
+
+    };
+
+
+    class DataMessage: public ControlMessage {
+
+    private:
+        uint64_t number_of_files;
+
+    public:
+        explicit DataMessage(uint64_t n_files):
+        ControlMessage(MessageType::DataMessage),
+        number_of_files(n_files) {}
+
+        void save(std::string filename) const final;
+        void load(std::string filename) const final;
+        [[nodiscard]] uint64_t get_number_of_files() const { return number_of_files; }
+
+    };
+
+
+}
+
+
+#endif //DISTRIBUTEDCOMPUTINGSYSTEM_CONTROLMESSAGE_H
